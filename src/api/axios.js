e9b80cf93d57ecf664cc841f753dbeb65e19569e
Devraj-098765 <119732347+Devraj-098765@users.noreply.github.com>
2025-03-01 11:57:36 +0545

    add axios instance with request interceptor for authentication

---
+++ src/api/axios.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("vetapp-token"); 
+    if (token) {
+      config.headers["x-auth-token"] = `${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
