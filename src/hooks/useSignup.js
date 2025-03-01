9574faf93b6ab2f1476cfae9c07cd4afdb61d7fa
Devraj-098765 <119732347+Devraj-098765@users.noreply.github.com>
2025-03-01 11:56:21 +0545

    add custom hook for user signup with loading and error handling

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import useAuth from "./useAuth";
+import axiosInstance from "../api/axios";
+
+const SIGNUP_URL = "/users";
+
+const useSignup = () => {
+  const { setAuth } = useAuth();
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleAuthSignup = async (data) => {
+    setLoading(true);
+    setError("");
+    try {
+      const response = await axiosInstance.post(SIGNUP_URL, data);
+      console.log("User created:", response.data);
+      const token = response.headers["x-auth-token"];
+
+      setAuth({
+        name: data.name,
+        email: data.email,
+        token,
+      });
+
+      toast.success("Account created Successfully");
+      navigate("/login");
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { handleAuthSignup, loading, error };
+};
+
+export default useSignup;
