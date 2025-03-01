5d455ea4655e4e795c38f2688177dd344d5df7fb
Devraj-098765 <119732347+Devraj-098765@users.noreply.github.com>
2025-03-01 11:53:04 +0545

    add custom hook for user login with error handling and success notifications

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import useAuth from "./useAuth";
+import axiosInstance from "../api/axios";
+
+const LOGIN_URL = "/auth";
+
+const useSignup = () => {
+  const { setAuth } = useAuth();
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleAuthLogin = async (data) => {
+    setLoading(true);
+    setError("");
+    try {
+      const response = await axiosInstance.post(LOGIN_URL, data);
+      const token = response.headers["x-auth-token"];
+
+      if (token) {
+        localStorage.setItem("vetapp-token", token);
+
+        setAuth({
+          email: data.email,
+          token,
+        });
+      }
+
+      toast.success("User Logged In successfully");
+      navigate("/");
+      localStorage.removeItem("currentConversationId");
+      localStorage.removeItem("currentConversationTitle");
+      setConversations(null);
+    } catch (err) {
+      if (err.response && err.response.data) {
+        if (err.response.data.message) {
+          setError(err.response.data.message);
+        }
+      } else if (err instanceof Error) {
+        setError(err.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { handleAuthLogin, loading, error };
+};
+
+export default useSignup;
