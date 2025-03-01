106ceec90ef2bfec97a8e12b13a5071f0815c291
Devraj-098765 <119732347+Devraj-098765@users.noreply.github.com>
2025-03-01 12:01:26 +0545

    add App component with routing and protected routes for authentication

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import MainLayout from "./component/MainLayout";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
+import BookingVisitForm from "./component/bookingsytem";
+import IndexPage from "./component/Index/IndexPage";
+import AboutUs from "./component/About/About";
+import Consultation from "./component/Consultation/Consultation";
+import Footer from "./component/Footer/Footer";
+import Contact from "./utils/Contact";
+import  Selectdate from "./component/Index/selectdate"
+import VetCareAdminDashboard from "../admin/Admin";
+
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Outlet /> {/* Renders the child routes */}
+        </>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <IndexPage />,
+        },
+        {
+          path: "/about-us",
+          element: <AboutUs />,
+        },
+        {
+          path: "/consultation",
+          element: <Consultation />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+        {
+          path: "/footer",
+          element: <Footer />,
+        },
+        {
+          path: "/BookingVisitForm",
+          element: (
+            <ProtectedRoute>
+              <BookingVisitForm />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: "/selectdate",
+          element:<Selectdate/>,
+        },
+      ],
+    },
+    {
+      path: "/sign-up",
+      element: <Signup />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/admin",
+      element: <VetCareAdminDashboard/>,
+    }
+
+    
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
