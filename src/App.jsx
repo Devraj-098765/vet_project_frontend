1fedf4680824b8b5114ad3360b87c60455fa954f
Devraj-098765 <119732347+Devraj-098765@users.noreply.github.com>
2025-03-23 11:53:44 +0545

    refactor(App): streamline routing structure and add AdminAppointments component

--- src/App.jsx
+++ src/App.jsx
@@ -1,127 +1,3 @@
-// import React from "react";
-// import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-// import ProtectedRoute from "./utils/ProtectedRoute";
-// import MainLayout from "./component/MainLayout";
-// import Login from "./pages/auth/Login";
-// import Signup from "./pages/auth/Signup";
-// import IndexPage from "./component/Index/IndexPage";
-// import AboutUs from "./component/About/About";
-// import Consultation from "./component/Consultation/Consultation";
-// import Footer from "./component/Footer/Footer";
-// import Contact from "./utils/Contact";
-// import Selectdate from "./component/Index/selectdate";
-// import Admin from "./pages/auth/Admin";
-// import AdminDashboard from "./component/admin/AdminDashboard";
-// import UserList from "./component/admin/Userlist/UserList";
-// import AddVet from "./component/admin/AddVet/AddVet.jsx";
-// import AdminNavbar from "./component/admin/AdminNavbar.jsx";
-// import VeterinarianList from "./component/admin/AddVet/VeterinarianList.jsx";
-// import UserVeterinarianList from "./component/UserVeterinarian/UserVeterinarianList.jsx";
-// import EditProfile from "./component/EditProfile/EditProfile.jsx";
-// import BookingSystem from "./component/Bookingsytem.jsx";
-// import AppointmentHistory from "./component/AppointmentHistory/AppointmentHistory.jsx"
-
-// function App() {
-//   const router = createBrowserRouter([
-//     {
-//       path: "/",
-//       element: (
-//         <>
-//           <Outlet /> {/* Renders the child routes */}
-//         </>
-//       ),
-//       children: [
-//         {
-//           path: "/",
-//           element: <IndexPage />,
-//         },
-//         {
-//           path: "/about-us",
-//           element: <AboutUs />,
-//         },
-//         {
-//           path: "/consultation",
-//           element: <Consultation />,
-//         },
-//         {
-//           path: "/user/veterinarians",
-//           element: <UserVeterinarianList/>
-
-//         },
-//         {
-//           path: "/contact",
-//           element: <Contact />,
-//         },
-//         {
-//           path: "/footer",
-//           element: <Footer />,
-//         },
-       
-//         {
-//           path: "/bookingsystem",
-//           element: (
-//             <ProtectedRoute>
-//               <BookingSystem />
-//             </ProtectedRoute>
-//           )
-//         },
-//         {
-//           path: "/editprofile/:id",
-//           element:<EditProfile/>
-
-//         },
-//         {
-//           path: "/appointmenthistory/:id",
-//           element: (
-//             <ProtectedRoute>
-//               <AppointmentHistory />
-//             </ProtectedRoute>
-//           ),
-//         },
-//       ],
-//     },
-//     {
-//       path: "/sign-up",
-//       element: <Signup />,
-//     },
-//     {
-//       path: "/login",
-//       element: <Login />,
-//     },
-//     {
-//       path: "/admin",
-//       element: <Admin />,
-//     },
-//     {
-//       path: "/adminDashboard",
-//       element: (
-//         <ProtectedRoute>
-//           <AdminDashboard />
-//         </ProtectedRoute>
-//       ),
-//     },
-//     {
-//       path: "/UserList",
-//       element: <UserList />
-//     },
-//     {
-//       path: "/AddVet",
-//       element: <AddVet />
-//     },
-//    {
-//     path: "/admin/veterinarianlist",
-//     element: <VeterinarianList />
-//    }
-//   ]);
-
-//   return <RouterProvider router={router} />;
-// }
-
-// export default App;
-
-
-
-// frontend/app.jsx
 import React from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import ProtectedRoute from "./utils/ProtectedRoute";
@@ -144,6 +20,7 @@ import UserVeterinarianList from "./component/UserVeterinarian/UserVeterinarianL
 import EditProfile from "./component/EditProfile/EditProfile.jsx";
 import BookingSystem from "./component/Bookingsytem.jsx";
 import AppointmentHistory from "./component/AppointmentHistory/AppointmentHistory.jsx";
+import AdminAppointments from "./component/admin/AdminAppointment/AdminAppointment.jsx";
 
 function App() {
   const router = createBrowserRouter([
@@ -151,34 +28,51 @@ function App() {
       path: "/",
       element: (
         <>
-          <Outlet />
+          <Outlet /> {/* Renders the child routes */}
         </>
       ),
       children: [
-        { path: "/", element: <IndexPage /> },
-        { path: "/about-us", element: <AboutUs /> },
-        { path: "/consultation", element: <Consultation /> },
-        { path: "/user/veterinarians", element: <UserVeterinarianList /> },
-        { path: "/contact", element: <Contact /> },
-        { path: "/footer", element: <Footer /> },
+        {
+          path: "/",
+          element: <IndexPage />,
+        },
+        {
+          path: "/about-us",
+          element: <AboutUs />,
+        },
+        {
+          path: "/consultation",
+          element: <Consultation />,
+        },
+        {
+          path: "/user/veterinarians",
+          element: <UserVeterinarianList/>
+
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+        {
+          path: "/footer",
+          element: <Footer />,
+        },
+       
         {
           path: "/bookingsystem",
           element: (
             <ProtectedRoute>
               <BookingSystem />
             </ProtectedRoute>
-          ),
+          )
         },
         {
           path: "/editprofile/:id",
-          element: (
-            <ProtectedRoute>
-              <EditProfile />
-            </ProtectedRoute>
-          ),
+          element:<EditProfile/>
+
         },
         {
-          path: "/appointmenthistory",
+          path: "/appointmenthistory/:id",
           element: (
             <ProtectedRoute>
               <AppointmentHistory />
@@ -187,9 +81,18 @@ function App() {
         },
       ],
     },
-    { path: "/sign-up", element: <Signup /> },
-    { path: "/login", element: <Login /> },
-    { path: "/admin", element: <Admin /> },
+    {
+      path: "/sign-up",
+      element: <Signup />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/admin",
+      element: <Admin />,
+    },
     {
       path: "/adminDashboard",
       element: (
@@ -198,12 +101,120 @@ function App() {
         </ProtectedRoute>
       ),
     },
-    { path: "/UserList", element: <UserList /> },
-    { path: "/AddVet", element: <AddVet /> },
-    { path: "/admin/veterinarianlist", element: <VeterinarianList /> },
+    {
+      path: "/appointments", 
+      element: (
+        <ProtectedRoute>
+          <AdminAppointments />
+        </ProtectedRoute>
+      )
+    },
+    {
+      path: "/UserList",
+      element: <UserList />
+    },
+    {
+      path: "/AddVet",
+      element: <AddVet />
+    },
+   {
+    path: "/admin/veterinarianlist",
+    element: <VeterinarianList />
+   }
   ]);
 
   return <RouterProvider router={router} />;
 }
 
 export default App;
+
+
+
+// // frontend/app.jsx
+// import React from "react";
+// import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+// import ProtectedRoute from "./utils/ProtectedRoute";
+// import MainLayout from "./component/MainLayout";
+// import Login from "./pages/auth/Login";
+// import Signup from "./pages/auth/Signup";
+// import IndexPage from "./component/Index/IndexPage";
+// import AboutUs from "./component/About/About";
+// import Consultation from "./component/Consultation/Consultation";
+// import Footer from "./component/Footer/Footer";
+// import Contact from "./utils/Contact";
+// import Selectdate from "./component/Index/selectdate";
+// import Admin from "./pages/auth/Admin";
+// import AdminDashboard from "./component/admin/AdminDashboard";
+// import UserList from "./component/admin/Userlist/UserList";
+// import AddVet from "./component/admin/AddVet/AddVet.jsx";
+// import AdminNavbar from "./component/admin/AdminNavbar.jsx";
+// import VeterinarianList from "./component/admin/AddVet/VeterinarianList.jsx";
+// import UserVeterinarianList from "./component/UserVeterinarian/UserVeterinarianList.jsx";
+// import EditProfile from "./component/EditProfile/EditProfile.jsx";
+// import BookingSystem from "./component/Bookingsytem.jsx";
+// import AppointmentHistory from "./component/AppointmentHistory/AppointmentHistory.jsx";
+// import AdminAppointments from "./component/admin/AdminAppointment/AdminAppointment.jsx";
+
+// function App() {
+//   const router = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: (
+//         <>
+//           <Outlet />
+//         </>
+//       ),
+//       children: [
+//         { path: "/", element: <IndexPage /> },
+//         { path: "/about-us", element: <AboutUs /> },
+//         { path: "/consultation", element: <Consultation /> },
+//         { path: "/user/veterinarians", element: <UserVeterinarianList /> },
+//         { path: "/contact", element: <Contact /> },
+//         { path: "/footer", element: <Footer /> },
+//         {
+//           path: "/bookingsystem",
+//           element: (
+//             <ProtectedRoute>
+//               <BookingSystem />
+//             </ProtectedRoute>
+//           ),
+//         },
+//         {
+//           path: "/editprofile/:id",
+//           element: (
+//             <ProtectedRoute>
+//               <EditProfile />
+//             </ProtectedRoute>
+//           ),
+//         },
+//         {
+//           path: "/appointmenthistory",
+//           element: (
+//             <ProtectedRoute>
+//               <AppointmentHistory />
+//             </ProtectedRoute>
+//           ),
+//         },
+//       ],
+//     },
+//     { path: "/sign-up", element: <Signup /> },
+//     { path: "/login", element: <Login /> },
+//     { path: "/admin", element: <Admin /> },
+//     {
+//       path: "/adminDashboard",
+//       element: (
+//         <ProtectedRoute>
+//           <AdminDashboard />
+//         </ProtectedRoute>
+//       ),
+//     },
+//     { path: "/UserList", element: <UserList /> },
+//     { path: "/AddVet", element: <AddVet /> },
+//     { path: "/admin/veterinarianlist", element: <VeterinarianList /> },
+//     { path: "/appointments", element: <AdminAppointments /> },
+//   ]);
+
+//   return <RouterProvider router={router} />;
+// }
+
+// export default App;
